296d5023badc984296d18ad44bc0d9a1b394c751
larsjaeger <lars@jgr.one>
2020-11-12 09:01:16 +0100

    initial commit, class creation

---
+++ src/main/java/one/jgr/listeners/KeyEvents.java
@@ -0,0 +1,45 @@
+package one.jgr.listeners;
+import java.awt.BorderLayout;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+import javax.swing.JFrame;
+import javax.swing.JTextField;
+
+public class KeyEvents extends JFrame implements KeyListener {
+    public KeyEvents(){
+        this.setLayout(new BorderLayout());
+        JTextField field = new JTextField();
+        field.addKeyListener(this);
+        this.add(field, BorderLayout.CENTER);
+        this.pack();
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setLocationRelativeTo(null);
+        this.setVisible(true);
+    }
+    @Override
+    public void keyTyped(KeyEvent e) {
+        System.out.println("KeyTyped: ");
+        if(e.getKeyChar() == KeyEvent.CHAR_UNDEFINED){
+            //TODO
+        }else{
+            //TODO
+        }
+        System.out.println("---");
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+        System.out.println("Taste: " + e.getKeyChar() + ", Code: " + e.getKeyCode());
+        System.out.println("Tastenposition: " + e.getKeyLocation());
+        System.out.println("---");
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+        if(e.getKeyCode() == KeyEvent.VK_SPACE){
+
+            // System.exit(0);
+        }
+    }
+}
---
+++ src/main/java/one/jgr/spaceInvaders/main/Main.java
@@ -0,0 +1,10 @@
+package one.jgr.spaceInvaders.main;
+
+import one.jgr.listeners.KeyEvents;
+
+public class Main {
+    public static void Main(String... args) {
+        // implement listeners
+        new KeyEvents();
+    }
+}
